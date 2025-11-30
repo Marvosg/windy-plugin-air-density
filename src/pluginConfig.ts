5a97f0cea78a50f0306845238b748613d2b3a52c
Marko Fekonja <marvosg@gmail.com>
2025-11-30 03:58:15 +0100

    Update air density plugin to version 1.2.0, change mobile UI from 'small' to 'fullscreen' for improved user experience.

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,14 +2,14 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-air-density',
-    version: '1.1.9',
+    version: '1.2.0',
     icon: '💨',
     title: 'Air Density',
     description: 'Displays air density calculated from temperature, pressure, and humidity.',
     author: 'Mare (lenient.fires_9q@icloud.com)',
     repository: 'https://github.com/Marvosg/windy-plugin-air-density/',
     desktopUI: 'rhpane',
-    mobileUI: 'small',
+    mobileUI: 'fullscreen',
     routerPath: '/air-density',
     desktopWidth: 400,
 };
